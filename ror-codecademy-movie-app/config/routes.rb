117ff4a3470866bb6ac3e6f09fe612a6bc8d6953
Jose David Baena <josedab@gmail.com>
2017-09-17 21:11:26 -0700

    RAILS-CODECADEMY Travel app: list and show movies and actors by id

--- ror-codecademy-movie-app/config/routes.rb
+++ ror-codecademy-movie-app/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get '/movies' => 'movies#index'
+	get '/movies/:id' => 'movies#show', as: :movie
+  get '/actors' => 'actors#index'
+  get '/actors/:id' => 'actors#show', as: :actor
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
