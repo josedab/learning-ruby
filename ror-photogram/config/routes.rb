0f1c04a765035375ae331c5d028287d89a865b27
Jose David Baena <josedab@gmail.com>
2017-09-23 19:34:17 -0700

    RAILS-PHOTOGRAM CRUD for posts - Controller, views and routes updated - Added ui for view, edit and delete posts - Refactored form code

--- ror-photogram/config/routes.rb
+++ ror-photogram/config/routes.rb
@@ -1,17 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/index'
 
-  get 'posts/new'
-
-  get 'posts/create'
-
-  get 'posts/show'
-
-  get 'posts/edit'
-
-  get 'posts/update'
-
-  get 'posts/destroy'
+  resources :posts
+  root :to => 'posts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
