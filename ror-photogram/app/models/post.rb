b1e430cb366e0ddcca7dc0573b4862e6972c4f6a
Jose David Baena <josedab@gmail.com>
2017-09-26 15:56:47 -0700

    RAILS-PHOTOGRAM Post validation for content and use of time_ago helper

--- ror-photogram/app/models/post.rb
+++ ror-photogram/app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   validates :image, presence: true
   validates :user_id, presence: true
+  validates :content, presence: true, length: { minimum: 3, maximum: 300 }
+
 
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
