d62812adc98af7fdbb0500bdd270d1c21e572c5d
Jose David Baena <josedab@gmail.com>
2017-09-25 19:38:11 -0700

    RAILS-PHOTOGRAM Users can own photos and photos have an user - Added belongs to and has_many to the appropiate models

--- ror-photogram/app/controllers/posts_controller.rb
+++ ror-photogram/app/controllers/posts_controller.rb
@@ -1,8 +1,9 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = current_user.posts.build
   end
 
   def show
@@ -13,11 +14,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    if @post = Post.create(post_params)
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
       flash[:success] = "Your post has been created!"
       redirect_to posts_path
     else
-      flash.now[:alert] = "Your new post couldn't be created!  Please check the form."
+      flash[:alert] = "Your new post couldn't be created!  Please check the form."
       render :new
     end
   end
